a1e181fe0e440c3aedac044334233149efb95403
JPedroBorges <JPedroBorges@users.noreply.github.com>
2022-11-05 16:11:15 +0000

    feat: initial spring commit

---
+++ toggle-manager/src/main/java/com/jpedroborges/fd/togglemanager/ToggleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jpedroborges.fd.togglemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToggleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToggleManagerApplication.class, args);
+	}
+
+}
---
+++ toggle-manager/src/test/java/com/jpedroborges/fd/togglemanager/ToggleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpedroborges.fd.togglemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToggleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
